3279add4f50b65e1555294c87eb327d2937bb9bc
Sawaki Shuto <swkshuto0208@icloud.com>
2023-09-27 14:48:33 +0900

    ResultList.swift

---
+++ safedrive/ResultList.swift
@@ -0,0 +1,20 @@
+//
+//  ResultList.swift
+//  safedrive
+//
+//  Created by 澤木柊斗 on 2023/09/27.
+//
+
+import SwiftUI
+
+struct ResultList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ResultList_Previews: PreviewProvider {
+    static var previews: some View {
+        ResultList()
+    }
+}
